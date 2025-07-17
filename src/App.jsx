cec892424af3c4a0f5794b874498e2e52cc32058
kimjh0378 <gwer2523@naver.com>
2025-07-17 17:36:46 +0900

    0717학원에서 한작업물

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,32 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Detail from './Detail';
+import App2 from './App2';
 
 function App() {
   // const [변수명, 변경함수] = useState(초기값);
   // useXXX : 리액트 내장함수 (리액트 훅)
   const[title, setTitle] = useState('상품목록');
+
+  const [board, setboard] = useState([
+    {
+      title : 'React',
+      data : '2025-07-15',
+      like : 0
+    },
+    {
+      title : 'HTML',
+      data : '2025-07-16',
+      like : 0
+    },
+    {
+      title : 'CSS',
+      data : '2025-07-13',
+      like : 0
+    },
+  ])
+
+
   const[boardTitle, setBoardTitle] = useState(['React', 'HTML', 'CSS']);
   const[like, setLike]= useState([0,0,0]);
 
@@ -15,6 +36,11 @@ function App() {
 
   // 몇번쨰 게시글을 클릭한지 저장
   const [titleIndex, settitleIndex] = useState(0);
+
+  //새로운글작성 제목을 기억하는 스테이트
+  const [newTitle, setNewTiltle] = useState('');
+
+  console.log(newTitle)
 
 
 
@@ -30,7 +56,10 @@ function App() {
 
 
   return (
+    
     <div className='App'>
+
+      <App2 />
       <div className='nav'>
         <h3>{title}</h3>
       </div> 
@@ -53,6 +82,16 @@ function App() {
                 setLike(_like);
               }}>좋아요</button> {like[i]} </h4>
               <p>2025-07-16</p>
+              <button onClick={()=> {
+                let _boardTitle = [...boardTitle];
+                _boardTitle.splice(i, 1);
+                setBoardTitle(_boardTitle);
+
+                let _like = [...like];
+                _like.splice(1, 1);
+                setLike(_like);
+
+              }}>삭제</button>
             </div>
           )
 
@@ -78,15 +117,33 @@ function App() {
       boardTitle={boardTitle}
        setBoardTitle={setBoardTitle}
         titleIndex={titleIndex}
-        
+
         /> : '' 
 
       }
         
-      
+        <br />
         
-        
-        
+        <input type="text" value={newTitle} onInput={(e) => {
+          setNewTiltle(e.target.value);
+        }} />
+        <button onClick={() => {
+          if( newTitle == '') {
+            alert('제목을 입력하셔야 합니다')
+            return;
+          }
+
+          let _boardTitle = [...boardTitle];
+          _boardTitle.push(newTitle);
+
+          setBoardTitle(_boardTitle);
+
+          let _like = [...like];
+          _like.push(0);
+          setLike(_like);
+
+          setNewTiltle('');
+        }}>글작성</button>
 
 
       
@@ -94,4 +151,4 @@ function App() {
   )
 }
 
-export default App
+export default App;
